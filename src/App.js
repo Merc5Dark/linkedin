20b3ed9dc1adf303a5fed47383209b9879a96c5d
Merc5Dark <31131794siya@gmail.com>
2023-08-10 15:44:55 +0200

    set-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
     return (
         <div className="App">
-            <h2>LinkedIn Clone</h2>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Login}></Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
 
 export default App;
+
 
